df9b3ba6102cb443d1600621053462d5c871635d
Nazar Alwi <alwinazar75@gmail.com>
2021-04-05 15:47:13 +0700

    Initial Commit

---
+++ EssentialFeedSecond/EssentialFeedSecond.h
@@ -0,0 +1,16 @@
+//
+//  Created by Nazar Alwi on 05/04/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedSecond.
+FOUNDATION_EXPORT double EssentialFeedSecondVersionNumber;
+
+//! Project version string for EssentialFeedSecond.
+FOUNDATION_EXPORT const unsigned char EssentialFeedSecondVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedSecond/PublicHeader.h>
+
+
