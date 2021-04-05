625ae6011d9641369f3bc35570d958a635ceaab6
Nazar Alwi <alwinazar75@gmail.com>
2021-04-05 16:31:40 +0700

    Create the FeedLoader protocol boundary

---
+++ EssentialFeedSecond/FeedLoader.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Nazar Alwi on 05/04/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
