d12ccaa6982e464b347f5db20d93f52a6f99c9e7
Nazar Alwi <alwinazar75@gmail.com>
2021-04-06 09:46:56 +0700

    `RemoteFeedLoader` does not request data upon creation

---
+++ EssentialFeedSecondTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,23 @@
+//
+//  Created by Nazar Alwi on 06/04/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
