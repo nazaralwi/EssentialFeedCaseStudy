de8b8e3b85f665df013a26668d9fc58612fe9ca9
Nazar Alwi <alwinazar75@gmail.com>
2021-05-04 09:54:12 +0700

    Move `FeedStore` to its own file

---
+++ EssentialFeedSecond/Feed Cache/FeedStore.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Nazar Alwi on 04/05/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
