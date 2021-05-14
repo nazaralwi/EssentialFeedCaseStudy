ee60ed982c761b4fe0ceb359830ae303b80f105f
Nazar Alwi <alwinazar75@gmail.com>
2021-05-14 13:54:05 +0700

    Fix typo and wrong method invocation

--- EssentialFeedSecondTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedSecondTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -31,13 +31,13 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
-    func test_load_doesNotDeletesCacheOnLessThanSevenDaysOldCache() {
+    func test_validateCache_doesNotDeleteLessThanSevenDaysOldCache() {
         let feed = uniqueImageFeed()
         let fixedCurrentDate = Date()
         let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
         let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
 
-        sut.load { _ in }
+        sut.validateCache()
         store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
