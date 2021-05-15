67cbdee5f31ebc601e612e4b763885ca5660cb1d
Nazar Alwi <alwinazar75@gmail.com>
2021-05-15 10:16:13 +0700

    Move `FeedCachePolicy` to its own file

---
+++ EssentialFeedSecond/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,23 @@
+//
+//  Created by Nazar Alwi on 15/05/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
