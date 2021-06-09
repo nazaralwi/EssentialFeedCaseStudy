880db3c7ef6c32d3b005fc0938f3d0bb6a6d6b24
Nazar Alwi <alwinazar75@gmail.com>
2021-06-09 17:34:40 +0700

    Add `EssentialFeediOS` framework (prod and test) targets for iOS platform-specific components

---
+++ EssentialFeedSecondiOS/EssentialFeedSecondiOS.h
@@ -0,0 +1,16 @@
+//
+//  Created by Nazar Alwi on 09/06/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedSecondiOS.
+FOUNDATION_EXPORT double EssentialFeedSecondiOSVersionNumber;
+
+//! Project version string for EssentialFeedSecondiOS.
+FOUNDATION_EXPORT const unsigned char EssentialFeedSecondiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedSecondiOS/PublicHeader.h>
+
+
