45576891edac4d8dac9bb809a721c7310dbec11c
Nazar Alwi <alwinazar75@gmail.com>
2021-06-11 14:31:07 +0700

    Render loaded feed on successful load completion

---
+++ EssentialFeedSecondiOS/FeedImageCell.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Nazar Alwi on 11/06/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
