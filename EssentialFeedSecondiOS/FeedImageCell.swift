d44aba9d8e7411f1d124498e6e75ba3d7c007a1f
Nazar Alwi <alwinazar75@gmail.com>
2021-06-14 17:13:24 +0700

    Feed image view retry button is visible on image url load error

--- EssentialFeedSecondiOS/FeedImageCell.swift
+++ EssentialFeedSecondiOS/FeedImageCell.swift
@@ -11,4 +11,5 @@ public final class FeedImageCell: UITableViewCell {
     public let descriptionLabel = UILabel()
     public let feedImageContainer = UIView()
     public let feedImageView = UIImageView()
+    public let feedImageRetryButton = UIButton()
 }
