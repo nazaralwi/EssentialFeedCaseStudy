1fa9c2e99252c9ad77e91cefa30545f47c6584d7
Nazar Alwi <alwinazar75@gmail.com>
2021-06-15 11:22:44 +0700

    Extract UIImage test helper to a separate file

---
+++ EssentialFeedSecondiOSTests/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Nazar Alwi on 15/06/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
+}
