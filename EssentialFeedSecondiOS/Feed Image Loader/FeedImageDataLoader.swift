c083ab1c8b28fa1b85b495b805a9a8d5d99cfb31
Nazar Alwi <alwinazar75@gmail.com>
2021-06-15 12:03:37 +0700

    Move `FeedImageDataLoader` to its own file

---
+++ EssentialFeedSecondiOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,16 @@
+//
+//  Created by Nazar Alwi on 15/06/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
