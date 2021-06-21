50d2cd5a58cb654fb93c678d35e033b53abdf3c4
Nazar Alwi <alwinazar75@gmail.com>
2021-06-21 16:33:08 +0700

    Move memory management to Composer layer. The `FeedPresenter` shouldn't have to know or handle their dependencies lifetime.

--- EssentialFeedSecondiOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeedSecondiOS/Feed Presentation/FeedPresenter.swift
@@ -23,7 +23,7 @@ final class FeedPresenter {
     }
     
     var feedView: FeedView?
-    weak var loadingView: FeedLoadingView?
+    var loadingView: FeedLoadingView?
     
     func loadFeed() {
         loadingView?.display(isLoading: true)
--- EssentialFeedSecondiOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeedSecondiOS/FeedUI/Composers/FeedUIComposer.swift
@@ -13,7 +13,7 @@ public final class FeedUIComposer {
         let feedPresenter = FeedPresenter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(presenter: feedPresenter)
         let feedController = FeedViewController(refreshController: refreshController)
-        feedPresenter.loadingView = refreshController
+        feedPresenter.loadingView = WeakRefVirtualProxy(refreshController)
         feedPresenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
         return feedController
     }
@@ -24,6 +24,20 @@ public final class FeedUIComposer {
                 return FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
             }
         }
+    }
+}
+
+private final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
     }
 }
 
