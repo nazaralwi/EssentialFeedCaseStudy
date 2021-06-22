72c61c68e56910451605739f9bc1e4b0e685f781
Nazar Alwi <alwinazar75@gmail.com>
2021-06-22 10:17:06 +0700

    Remove unused typealias

--- EssentialFeedSecondiOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeedSecondiOS/Feed Presentation/FeedPresenter.swift
@@ -22,8 +22,6 @@ protocol FeedView {
 }
 
 final class FeedPresenter {
-    typealias Observer<T> = (T) -> Void
-    
     private let feedLoader: FeedLoader
     
     init(feedLoader: FeedLoader) {
