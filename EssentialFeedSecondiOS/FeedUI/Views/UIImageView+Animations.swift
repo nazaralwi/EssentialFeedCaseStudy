45c15b839e90833593ceed012864a754353afed1
Nazar Alwi <alwinazar75@gmail.com>
2021-06-23 15:37:45 +0700

    Display image with animation

---
+++ EssentialFeedSecondiOS/FeedUI/Views/UIImageView+Animations.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Nazar Alwi on 23/06/21
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
