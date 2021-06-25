67f0cf7384b632b28fba4df3751c018f1cacf217
Nazar Alwi <alwinazar75@gmail.com>
2021-06-25 15:14:52 +0700

    Move `MainQueueDispatchDecorator` to separate file

---
+++ EssentialFeedSecondiOS/Feed UI/Composers/MainQueueDispatchDecorator.swift
@@ -0,0 +1,42 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  EssentialFeedSecondiOS
+//
+//  Created by Nazar Alwi on 25/06/21.
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import UIKit
+import EssentialFeedSecond
+
+final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { completion() }
+        }
+        
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
+
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
