d95fd45f957c2a0cee5f127c1beb42402fdb19bc
Nazar Alwi <alwinazar75@gmail.com>
2021-06-29 11:51:14 +0700

    Move `ErrorView` to separate file

---
+++ EssentialFeedSecondiOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorView.swift
+//  EssentialFeedSecondiOS
+//
+//  Created by Nazar Alwi on 29/06/21.
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
