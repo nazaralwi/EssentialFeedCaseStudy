9cbbf65a54b5d7c9cf299202cc66f492ffcef6bf
Nazar Alwi <alwinazar75@gmail.com>
2021-06-29 12:53:28 +0700

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeedSecondiOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeedSecondiOS
+//
+//  Created by Nazar Alwi on 29/06/21.
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+    let message: String?
+}
