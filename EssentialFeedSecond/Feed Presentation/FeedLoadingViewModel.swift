65ab9439da854807c3cb9e4fe21f14f7b6c725cd
Nazar Alwi <alwinazar75@gmail.com>
2021-07-01 10:34:42 +0700

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeedSecond/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeedSecond
+//
+//  Created by Nazar Alwi on 01/07/21.
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
