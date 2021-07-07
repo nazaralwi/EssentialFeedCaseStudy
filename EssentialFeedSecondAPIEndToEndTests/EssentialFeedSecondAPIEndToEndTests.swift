5f04c1aaad811c2f084d61bdec2e0462295e2ce6
Nazar Alwi <alwinazar75@gmail.com>
2021-07-07 09:41:41 +0700

    Add `RemoteFeedImageDataLoader` end-to-end test to guarantee both the client (app) and server (backend) respects the API contract, so we can fetch image data from remote.

--- EssentialFeedSecondAPIEndToEndTests/EssentialFeedSecondAPIEndToEndTests.swift
+++ EssentialFeedSecondAPIEndToEndTests/EssentialFeedSecondAPIEndToEndTests.swift
@@ -26,6 +26,19 @@ class EssentialFeedSecondAPIEndToEndTests: XCTestCase {
         }
     }
     
+    func test_endToEndTestServerGETFeedImageDataResult_matchesFixedTestAccountData() {
+        switch getFeedImageResult() {
+        case let .success(data)?:
+            XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+            
+        case let .failure(error)?:
+            XCTFail("Expected successful image data result, got \(error) instead")
+            
+        default:
+             XCTFail("Expected successful image data result, got no result instead")
+        }
+    }
+    
     // MARK: - Helpers
     
     private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
@@ -39,6 +52,25 @@ class EssentialFeedSecondAPIEndToEndTests: XCTestCase {
         
         var receivedResult: FeedLoader.Result?
         loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        return receivedResult
+    }
+    
+    private func getFeedImageResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let loader = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: FeedImageDataLoader.Result?
+        _ = loader.loadImageData(from: testServerURL) { result in
             receivedResult = result
             exp.fulfill()
         }
