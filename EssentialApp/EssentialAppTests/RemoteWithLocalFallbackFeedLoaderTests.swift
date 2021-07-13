a711bad08e99bf5e9222a822eb3c6ea32790e4db
Nazar Alwi <alwinazar75@gmail.com>
2021-07-13 11:27:34 +0700

    Configure test setup

---
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by Nazar Alwi on 13/07/21.
+//
+
+import XCTest
+
+class RemoteWithLocalFallbackFeedLoaderTests: XCTestCase {
+
+}
