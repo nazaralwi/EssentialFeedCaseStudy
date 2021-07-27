e9c2ec97b157f070d95c16031959e32302ea6edc
Nazar Alwi <alwinazar75@gmail.com>
2021-07-27 08:25:04 +0700

    Record empty feed snapshot

---
+++ EssentialFeedSecondiOSTests/FeedSnapshotTests.swift
@@ -0,0 +1,66 @@
+//
+//  FeedSnapshotTests.swift
+//  EssentialFeedSecondiOSTests
+//
+//  Created by Nazar Alwi on 26/07/21.
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import XCTest
+import EssentialFeedSecondiOS
+
+class FeedSnapshotTests: XCTestCase {
+    func test_emptyFeed() {
+        let sut = makeSUT()
+        
+        sut.display(emptyFeed())
+        
+        record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let stodyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = stodyboard.instantiateInitialViewController() as! FeedViewController
+        controller.loadViewIfNeeded()
+        return controller
+    }
+    
+    private func emptyFeed() -> [FeedImageCellController] {
+        return []
+    }
+    
+    private func record(snapshot: UIImage, named name: String, file: StaticString = #filePath, line: UInt = #line) {
+        guard let snapshotData = snapshot.pngData() else {
+            XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
+            return
+        }
+        
+        let snapshotURL = URL(fileURLWithPath: String(describing: file))
+            .deletingLastPathComponent()
+            .appendingPathComponent("snapshots")
+            .appendingPathComponent("\(name).png")
+        
+        do {
+            try FileManager.default.createDirectory(
+                at: snapshotURL.deletingLastPathComponent(),
+                withIntermediateDirectories: true
+            )
+            
+            try snapshotData.write(to: snapshotURL)
+        } catch {
+            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+        }
+    }
+}
+
+extension UIViewController {
+    func snapshot() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+        return renderer.image { action in
+            view.layer.render(in: action.cgContext)
+        }
+    }
+}
