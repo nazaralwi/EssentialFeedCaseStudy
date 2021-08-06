6776f19f2e9ad28ba927df494706c2c1c247d165
Nazar Alwi <alwinazar75@gmail.com>
2021-08-06 12:01:42 +0700

    Fix bug in retry button

--- EssentialFeedSecondiOS/Feed UI/Views/FeedImageCell.swift
+++ EssentialFeedSecondiOS/Feed UI/Views/FeedImageCell.swift
@@ -10,13 +10,8 @@ public final class FeedImageCell: UITableViewCell {
     @IBOutlet private(set) public var locationLabel: UILabel!
     @IBOutlet private(set) public var descriptionLabel: UILabel!
     @IBOutlet private(set) public var feedImageContainer: UIView!
+    @IBOutlet private(set) public var feedImageRetryButton: UIButton!
     @IBOutlet private(set) public var feedImageView: UIImageView!
-    
-    private(set) public lazy var feedImageRetryButton: UIButton = {
-        let button = UIButton()
-        button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
-        return button
-    }()
     
     var onRetry: (() -> Void)?
     
