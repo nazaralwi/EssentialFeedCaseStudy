888130b56ae3770edfdd590025ef315ff9a664a1
Nazar Alwi <alwinazar75@gmail.com>
2021-08-10 12:14:17 +0700

    Delivers proper results on 2xx response

--- EssentialFeedSecond/Feed API/ImageCommentsMapper.swift
+++ EssentialFeedSecond/Feed API/ImageCommentsMapper.swift
@@ -12,15 +12,16 @@ final class ImageCommentsMapper {
     private struct Root: Decodable {
         let items: [RemoteFeedItem]
     }
-    
-    private static var OK_200: Int { return 200 }
-    
+        
     static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
-        guard response.statusCode == OK_200,
-            let root = try? JSONDecoder().decode(Root.self, from: data) else {
-                throw RemoteImageCommentsLoader.Error.invalidData
+        guard isOK(response), let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
         }
 
         return root.items
+    }
+    
+    private static func isOK(_ response: HTTPURLResponse) -> Bool {
+        (200...299).contains(response.statusCode)
     }
 }
