024aa7f95d093fe1805c974b98d5bdb8fa67120b
Nazar Alwi <alwinazar75@gmail.com>
2021-08-20 11:17:03 +0700

    Remove unused `FeedImagePresenter` logic

--- EssentialFeedSecond/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeedSecond/Feed Presentation/FeedImageViewModel.swift
@@ -6,20 +6,11 @@
 //  Copyright © 2021 Nazar Alwi. All rights reserved.
 //
 
-public struct FeedImageViewModel<Image> {
+public struct FeedImageViewModel {
     public let description: String?
     public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
     
     public var hasLocation: Bool {
         return location != nil
     }
-}
-
-public protocol FeedImageView {
-    associatedtype Image
-    
-    func display(_ model: FeedImageViewModel<Image>)
 }
