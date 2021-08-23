6797d808afcd5d6c82b14ad4fff6b103363a9e15
Nazar Alwi <alwinazar75@gmail.com>
2021-08-23 10:27:14 +0700

    Extract shared `ListViewController` snapshot tests

---
+++ EssentialFeedSecondiOSTests/Shared UI/ListSnapshotTests.swift
@@ -0,0 +1,45 @@
+//
+//  ListSnapshotTests.swift
+//  EssentialFeedSecondiOSTests
+//
+//  Created by Nazar Alwi on 23/08/21.
+//  Copyright © 2021 Nazar Alwi. All rights reserved.
+//
+
+import XCTest
+import EssentialFeedSecondiOS
+@testable import EssentialFeedSecond
+
+class ListSnapshotTests: XCTestCase {
+    func test_emptyList() {
+        let sut = makeSUT()
+        
+        sut.display(emptyList())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_LIST_dark")
+    }
+    
+    func test_listWithErrorMessage() {
+        let sut = makeSUT()
+        
+        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+        
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+    }
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let stodyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = stodyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func emptyList() -> [CellController] {
+        return []
+    }
+}
